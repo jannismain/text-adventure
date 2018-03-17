ca8b21a824163ed227a90779760aad2d1e7744b4
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2018-03-17 13:50:42 +0100

    renamed things to items

--- Inventory.py
+++ Inventory.py
@@ -10,23 +10,41 @@ class Inventory:
     def check_inventory(self):
         print(self.inventory)
 
-    def add(self, thing: str):
-        """Add things to your inventory."""
+    def add(self, item: str):
+        """Add items to your inventory."""
         if len(self.inventory) < self.size:
-            self.inventory.append(thing)
-            print("You added {} to your inventory!".format(thing))
+            self.inventory.append(item)
+            print("You added {} to your inventory!".format(item))
         else:
             print("Your inventory is full. Discard an item before you can add new ones!")
 
-    def remove(self, thing: str):
-        """Remove things from your inventory."""
-        if thing in self.inventory:
-            self.inventory.remove(thing)
+    def remove(self, item: str):
+        """Remove items from your inventory."""
+        if item in self.inventory:
+            self.inventory.remove(item)
         else:
-            print("Beep! {} is not in your inventory!".format(thing))
+            print(f"Beep! {item} is not in your inventory!")
 
-class Thing:
-    """Thing class for inventory."""
-    
-    def __init__(self, name):
+    def __str__(self):
+        if len(self.inventory) == 0:
+            return "Your Inventory is empty!"
+        else:
+            return f"Inventory: {str(self.inventory)}"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Item:
+    """An item represents an object in the `Inventory`."""
+
+    def __init__(self, name, description, is_obtainable):
         self.name = name
+        self.description = description
+        self.is_obtainable = is_obtainable
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
