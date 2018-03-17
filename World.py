77bb56de74aaeca7699b8b73e798310729ead95c
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2018-03-17 13:53:01 +0100

    visited flag per tile

--- World.py
+++ World.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import csv
+import logging
 
 DIRECTIONS = {
     'n': 'north',
@@ -16,9 +17,8 @@ class World:
     def __init__(self, data: str):
         self.tiles, self.current_tile = self.load(data)
         [tile.discover_tiles(self) for tile in self.tiles]
-        # print("Places: ", self.tiles)
-        # print(self.current_tile)
-    
+        logging.debug(f"Loaded Tiles: {self.tiles}")
+
     @staticmethod
     def load(datafile: str):
         tiles = []
@@ -33,15 +33,17 @@ class World:
                     print("A minimal place description has the following 5 entries: name, north_exit, east_exit, south_exit, west_exit")
                     print("Additional entries are considered point of interests or items available for closer examination.")
                 except AssertionError:
-                    print(f"Beep! Place '{datarow[0]}' does not seem to be a valid place. It is not connected to any other tile at the moment.")
-                
+                    print(
+                        f"Beep! Place '{datarow[0]}' does not seem to be a valid place. It is not connected to any other tile at the moment.")
+
                 if datarow[0].endswith("*"):
                     if start_at == "":
                         start_at = tiles[-1]
                     else:
-                        raise AttributeError("Data contains more than one start tile. Start tiles are indicated by a '*' at the end of a place name.")
+                        raise AttributeError(
+                            "Data contains more than one start tile. Start tiles are indicated by a '*' at the end of a place name.")
         return tiles, start_at
-    
+
     def move(self, direction: str):
         """Move from the current tile in the specified direction (if valid)."""
         if len(direction) == 1:
@@ -52,10 +54,11 @@ class World:
         new_tile = self.current_tile.surrounding[direction]
         if new_tile:
             self.current_tile = new_tile
-            print("You entered '{}'.".format(self.current_tile.name))
+            self.current_tile.visited = True
+            print("You moved to '{}'.".format(self.current_tile.name))
         else:
             print("Beep! This move is not available!")
-    
+
     def get_tile(self, name):
         for tile in self.tiles:
             if tile.name == name:
@@ -69,10 +72,12 @@ class Tile:
     def __init__(self, name, north, east, south, west, description=""):
         if name.endswith("*"):
             self.name = name[:-1]
+            self.visited = True
         else:
             self.name = name
+            self.visited = False
         self.description = description
-        
+
         self.surrounding = {
             "north": north,
             "east": east,
@@ -80,20 +85,21 @@ class Tile:
             "west": west
         }
         self.test_tile_validity()
-    
-    def get_exits(self):
+
+    @property
+    def exits(self):
         return [dir for (dir, tile) in self.surrounding.items() if tile != None]
-    
+
     def test_tile_validity(self):
         assert(any([tile for tile in self.surrounding.items() if tile != "None"]))
-    
+
     def discover_tiles(self, world: World):
         for dir, tile_desc in self.surrounding.items():
             self.surrounding[dir] = world.get_tile(tile_desc)
-    
+
     def __str__(self):
         return self.name
-    
+
     def __repr__(self):
         return self.__str__()
 
