2f8c719cd081db5fa7c5e2cf9c2b3cfbe12215a8
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2018-06-16 13:37:49 +0200

    cloning src to tutorial and revert state to current tutorial step

---
+++ tutorial/src/Textadventure.py
@@ -0,0 +1,17 @@
+from World import World
+from Player import Player
+
+
+class Textadventure:
+
+    def __init__(self):
+        self.world = World()
+        self.player = Player()
+        self.main_loop()
+
+    def main_loop(self):
+        pass
+
+
+if __name__ == '__main__':
+    Textadventure()
---
+++ tutorial/src/World.py
@@ -0,0 +1,15 @@
+class Tile:
+
+    def __init__(self, name: str, description: str, *args)
+        self.name = name
+        self.description = description
+
+        # either save surrounding tiles as list
+        # self.surroundings = args
+        # ... or save surrounding tiles as dictionary
+        self.surroundings = {
+            "north" = args[0],
+            "east" = args[1],
+            "south" = args[2],
+            "west" = args[3]
+        }
