71ac6b0ff41194016a164a4092d129a6d348a2d9
Jannis Mainczyk <mainczjs@iis.fraunhofer.de>
2018-06-16 13:52:29 +0200

    adding stubs so code is executable

--- tutorial/src/World.py
+++ tutorial/src/World.py
@@ -1,6 +1,12 @@
+class World:
+
+    def __init__(self):
+        pass
+
+
 class Tile:
 
-    def __init__(self, name: str, description: str, *args)
+    def __init__(self, name: str, description: str, *args):
         self.name = name
         self.description = description
 
@@ -8,8 +14,8 @@ class Tile:
         # self.surroundings = args
         # ... or save surrounding tiles as dictionary
         self.surroundings = {
-            "north" = args[0],
-            "east" = args[1],
-            "south" = args[2],
-            "west" = args[3]
+            "north": args[0],
+            "east": args[1],
+            "south": args[2],
+            "west": args[3]
         }
